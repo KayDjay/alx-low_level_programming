abbdbeabf6ea1f766555195f42edb6c448aee063
KayDjay <Khadijatrasaq@gmail.com>
2023-03-06 09:55:32 -0800

    Chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - Function that locate a string
+ * @haystack: input
+ * @needle: input
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int ;
+
+
