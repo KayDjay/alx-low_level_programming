7284541c8a9e133cf61c77738ab90e5579da7494
KayDjay <Khadijatrasaq@gmail.com>
2023-03-06 20:50:56 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,6 +9,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int ;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
 
