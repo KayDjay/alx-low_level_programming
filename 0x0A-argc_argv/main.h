a7917c5591cf2df906979627403a8041f6f06267
KayDjay <Khadijatrasaq@gmail.com>
2023-03-15 09:25:55 -0700

    Multiplication

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, __attribute__((unused)) char *argv[]);
 int change(int cents);
+int _atoi(char *s);
 
 #endif
