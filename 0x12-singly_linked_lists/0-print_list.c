b3776185e0f03ad208eda28abc4fe330f6fbf114
KayDjay <Khadijatrasaq@gmail.com>
2023-03-30 19:47:46 -0700

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,19 @@
  *
  * Return: the number of nodes printed
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t a = 0;
 
-	while (h)
+	while (h != 0)
 	{
 		if (!h->str)
-		printf("[0] (nil)\n");
+		printf("[0] (nill)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
 		 h = h->next;
-		 s++;
+		 a++;
 	}
-	return (s);
+	return (a);
 }
