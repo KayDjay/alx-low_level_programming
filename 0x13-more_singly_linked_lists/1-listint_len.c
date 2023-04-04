4e7bae04bcf3cc9d15840ca50b39e99b5b513b2c
KayDjay <Khadijatrasaq@gmail.com>
2023-04-04 04:18:21 -0700

    Add node

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,13 @@
+#include "list.h"
+
+/**
+ * size_t listint_len - returns the number of elements in a linked list
+ *
+ * @h: node head
+ *
+ * Return: number of num
+ */
+
+size_t listint_len(const listint_t *h)
+{
+
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,15 @@
+#include "list.h"
+
+/**
+ * add_nodeint - To add new node at the beginning of a list
+ * @head: node head
+ * @n: new node integer
+ *
+ * Return: the address of the new node
+ *or Null if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+ 
+}
