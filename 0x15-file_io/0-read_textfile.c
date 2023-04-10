ebb11583c204b670f0fddeebb004c9aea19fe174
KayDjay <Khadijatrasaq@gmail.com>
2023-04-10 14:41:48 -0700

    Read Textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@ include "main.h"
 /**
  * read_textfile - A function that read a text
  *
- * @filename: file to b open or read
+ * @filename: file to be open or read
  * @letters: content of text in the file
  *
  * Return: numbers of letter and print
@@ -12,27 +12,27 @@ include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        ssize_t o, r, w;
-        char *buffer;
+	ssize_t o, r, w;
+	char *buffer;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(sizeof(char) * letters);
-        if (buffer == NULL)
-                return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-        o = open(filename, O_RDONLY);
-        r = read(op, buffer, letters);
-        w = write(STDOUT_FILENO, buffer, re);
+	o = open(filename, O_RDONLY);
+	r = read(op, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, re);
 
-        if (o == -1 || r == -1 || w == -1 || wri != r)
-        {
-                free(buffer);
-                return (0);
-        }
+	if (o == -1 || r == -1 || w == -1 || wri != r)
+	{
+	free(buffer);
+	return (0);
+	}
 
-        free(buffer);
-        close(o);
+	free(buffer);
+	close(o);
 
-        return (w);
+	return (w);
