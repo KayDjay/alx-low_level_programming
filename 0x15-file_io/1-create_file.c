8726192c6a220b31c228d070d7de5c17cbab414c
KayDjay <Khadijatrasaq@gmail.com>
2023-04-13 05:30:42 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - Function that creates a file
  *
- * @filename: name of the file
+ * @filename: name of the file to be created
  * @text_content: content of the file
  *
  * Return: (-1) if failed, filename
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (; text_content[len];)
-				len++;
+		for (len = 0; text_content[len]; )
+			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w  == -1)
