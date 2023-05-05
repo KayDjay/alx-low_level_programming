b545b6fa0781316b0d54f85eca794e6b6c45e639
root <Khadijatrasaq@gmail.com>
2023-05-05 06:48:56 -0700

    Clear bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,20 +6,20 @@
  * @n: number of bit
  * @index: index of bit
  *
- * Return: value of bit at index
+ * Return: 0
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int a = 0;
 
-	if (n == 0 && index < 64)
+	if (n == 0 && index == 0)
 		return (0);
 
-	for (; a <= 63; n >>= 1, a++)
+	for (a = 0; a <= 63; n >>= 1, a++)
 	{
 		if (index == a)
 			return (n && 1);
 	}
-	return (a);
+	return (-1);
 }
