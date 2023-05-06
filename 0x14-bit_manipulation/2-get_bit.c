0140de992c27ed991d03f5fdaf60fcd84c1a0349
root <Khadijatrasaq@gmail.com>
2023-05-05 21:08:08 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,12 +14,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int a = 0;
 
 	if (n == 0 && index == 0)
-		return (0);
+		return (-1);
 
-	for (a = 0; a <= 63; n >>= 1, a++)
-	{
-		if (index == a)
-			return (n && 1);
-	}
-	return (-1);
+	a = (n >> index) & 1;
+
+	return (a);
 }
