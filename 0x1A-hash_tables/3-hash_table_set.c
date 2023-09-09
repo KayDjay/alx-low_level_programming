8f435ecda2b8fb9d75bd8f5ac3600abb9060cd93
root <Khadijatrasaq@gmail.com>
2023-09-08 17:35:45 -0700

    Task 4&5

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table-set - function to adds an element to the hash table
+ * hash_table_set - function to adds an element to the hash table
  *
  * @ht: head pointer to hash table
  * @key: key to hash table
@@ -22,21 +22,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	idx = key_index((unsigned char *)key, ht->size);
 
-	if (update_hash(&(ht->array[idx]), key, value) == NULL)
+	if (add_hash(&(ht->array[idx]), key, value) == NULL)
 		return (0);
 
 	return (1);
 }
 
 /**
- * update_hash - update the addition of hash table
+ * add_hash - update the addition of hash table
  *
  * @node: node to set linked list
  * @key: key of the hash
  * @value: value to store in the key
  * Return: head of the hash
  */
-hash_node_t *update_hash(hash_node_t **node, const char *key, const char *value)
+hash_node_t *add_hash(hash_node_t **node, const char *key, const char *value)
 {
 	hash_node_t *tmp;
 
